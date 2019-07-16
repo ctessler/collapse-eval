19b8ed3c8704e8aa30b18b515d96bf06f0da94c5
Corey Tessler <corey.tessler@wayne.edu>
2019-07-16 12:43:30 -0400

    Added the utilization across all core allocations generation

---
+++ bash/sched-sum.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+declare TOTAL		# Total number of tasks
+declare NC_COUNT	# No collapse count
+declare CA_COUNT	# Arbitrary count
+declare CB_COUNT	# Max Benefit count
+declare CP_COUNT	# Min Penalty count
+
+echo "SCHEDULABILITY RESULTS"
+TOTAL=$(grep -v \# sched.dat | wc -l)
+echo "Number of Task Sets: $TOTAL"
+NC_COUNT=$(grep -v \# sched.dat | awk '{print $3}' | sed -n /yes/p | wc -l)
+NA_COUNT=$(grep -v \# sched.dat | awk '{print $4}' | sed -n /yes/p | wc -l)
+NB_COUNT=$(grep -v \# sched.dat | awk '{print $5}' | sed -n /yes/p | wc -l)
+NP_COUNT=$(grep -v \# sched.dat | awk '{print $6}' | sed -n /yes/p | wc -l)
+
+function pct {
+	local p=$(echo "$1 / $2" | bc -l)
+
+	printf "%0.2f" $p
+}
+
+echo "Schedulable with collapse heuristic:"
+echo "None		$NC_COUNT" $(pct $NC_COUNT $TOTAL) "%"
+echo "Arbitrary	$NA_COUNT" $(pct $NA_COUNT $TOTAL) "%"
+echo "Max Ben.	$NB_COUNT" $(pct $NB_COUNT $TOTAL) "%"
+echo "Min Pen.	$NP_COUNT" $(pct $NP_COUNT $TOTAL) "%"
+
---
+++ bash/util-sum.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my $USAGE = << 'EOM';
+Usage: util-sum.pl <FILE>
+EOM
+
+# False entrypoint for perl
+sub main {
+    my %opts = ( file => "",
+		 tgtu => 0.0,
+		 jitr => 5
+    );
+
+    GetOptions("file=s" => \$opts{file},
+	       "util=f" => \$opts{tgtu},
+	)
+	or die($USAGE);
+    $opts{file} = shift @ARGV;
+
+    my %data;
+    open(my $fh, '<', $opts{file}) or die ("Could not open $opts{file}\n");
+    while (my $line = <$fh>) {
+	chomp($line);
+	if ($line =~ /^#/) {
+	    next;
+	}
+	$line =~ s/^\s+//g;
+	my ($task, $util);
+	($task, $util) = split(/\s+/, $line);
+	push @{$data{$util}}, $task;
+    }
+    close ($fh);
+    my %sched;
+    %sched = read_sched('sched.dat');
+
+    printf "#%4s %3s %3s %3s %3s\n", 'UTIL', 'NC', 'CA', 'CB', 'CP';
+    my @utils = sort {$a <=> $b} keys %data;
+    foreach my $u (@utils) {
+	my ($sc, $sa, $sb, $sp);
+	$sc = $sa = $sb = $sp = 0;
+	foreach my $t (@{$data{$u}}) {
+	    foreach my $c (keys %{$sched{$t}}) {
+		$sc += $sched{$t}{$c}{sc};
+		$sa += $sched{$t}{$c}{sa};
+		$sb += $sched{$t}{$c}{sb};
+		$sp += $sched{$t}{$c}{sp};		
+	    }
+	}
+	printf "%5.2f %3d %3d %3d %3d\n", $u, $sc, $sa, $sb, $sp;
+    }
+	
+    return 0;
+}
+
+sub read_sched {
+    my $file = shift;
+    open(my $fh, '<', $file) or die ("Could not open $file\n");
+    
+    my %rv;
+    while (my $line = <$fh>) {
+	if ($line =~ /^#/) {
+	    next;
+	}
+	$line =~ s/^\s+//g;
+	my ($task, $cores, $sc, $sa, $sb, $sp);
+	($task, $cores, $sc, $sa, $sb, $sp) = split(/\s+/, $line);
+
+	$rv{$task}{$cores}{sc} = $sc eq "yes" ? 1 : 0;
+	$rv{$task}{$cores}{sa} = $sa eq "yes" ? 1 : 0;
+	$rv{$task}{$cores}{sb} = $sb eq "yes" ? 1 : 0;	
+	$rv{$task}{$cores}{sp} = $sp eq "yes" ? 1 : 0;
+    }
+
+    close($fh);
+    return %rv;
+}
+
+exit main()
+
