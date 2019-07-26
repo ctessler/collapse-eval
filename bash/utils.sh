73a0bc3244c4d5573a6945da19ab99929ea9937b
Corey <corey@n00b.net>
2019-07-26 11:27:09 -0400

    clean up utils.sh

--- bash/utils.sh
+++ bash/utils.sh
@@ -6,14 +6,13 @@ source ${contdir}/funcs.sh
 export GLS_RNG_TYPE=ranlxs2
 inc_seed
 
-declare COMB=$(deadlinec)
+declare COMB=$(find ../trim -name "*.dot" | wc -l)
 declare LOG=util.log
 declare START=$(date +%s)
 
 # False entrypoint
 function main {
-	report
-	begin_osect "UTIL[$COMB]"
+	begin_osect "UTIL[$COMB] "
 	local n
 	rm -f unsorted.util
 	rm -f sorted.util
@@ -30,16 +29,6 @@ function main {
 	echo "Duration: $mins m Log: $LOG"
 	
 	return 0;
-}
-
-function report {
-	echo "Creating base graphs with parameters:"
-	echo -e "\tNodes:\t${NODES[@]} ${#NODES[@]}"
-	echo -e "\tEdegeP:\t${EDGEP[@]}"
-	echo -e "\tOjbs:\t${OBJS[@]}"
-	echo -e "\tGrowF:\t${GROWF[@]}"	
-	echo -e "\tUtils:\t${UTILS[@]}"
-	echo -e "\tJobs:\t$JOBS"
 }
 
 main
