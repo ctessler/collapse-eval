60e999f7ba5c264d90f01ab41fd5460a6395b212
Corey Tessler <corey.tessler@wayne.edu>
2019-07-30 20:45:02 -0400

    Increase the taskset parameters

--- bash/params.sh
+++ bash/params.sh
@@ -8,9 +8,9 @@ JOBS=${JOBS:-4}
 TCNT=100
 
 # Total task set utilization
-TASKSET_UTILS=(${TASKSET_UTILS[@]:-0.5 1 2 4 8 16 32})
+TASKSET_UTILS=(${TASKSET_UTILS[@]:-0.5 1 2 4 8 16 20 24 28 32 36})
 
 # Architecture Parameters
-CORES=(${CORES[@]:-4 8 12 16 20 24 28 32})
+CORES=(${CORES[@]:-2 4 8 12 16 20 24 28 32})
 
 
