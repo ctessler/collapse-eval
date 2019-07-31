f790a8a6755f4004ea5b87eda7e7726a6da2dd39
Corey Tessler <corey.tessler@wayne.edu>
2019-07-30 21:28:44 -0400

    Final parameters

--- params.mk
+++ params.mk
@@ -1,23 +1,18 @@
 JOBS := 4
+# Schedulability tests get no more than 15 minutes each.
+TIMEOUT := 10
+
 # Pipeline Stage 1, Graph Creation
 NODES := 4 8 12 16 20 24 28 32
-NODES := 4 8 12 
-EDGEP := 0.02 0.04 0.06 0.08. 0.10 0.12 0.14 0.16 0.18 0.20
-EDGEP := 0.02 0.04 0.06 
-GRAFC := 100
-GRAFC := 3
+EDGEP := 0.02 0.06 0.10 0.14 0.18
+GRAFC := 20
 
 # Pipeline Stage 1, Demand Parameters
 WCET1 := 50
-OBJS  := 2 4 8 12 14 6
-OBJS  := 2 4
+OBJS  := 4 8 12 14 16
 GROWF := 0.2 0.4 0.6 0.8 1.0
-GROWF := 0.2 0.4
-CPFAC := 0.5 1.0 2.0 4.0 8.0 16.0
-CPFAC := 0.5 2.0 16.0
-UTILS := 0.2 0.4 0.6 0.8 1.0 1.5 2.0 3.0 4.0 5.0 6.0 7.0 8.0
-UTILS := 0.2 4.0 8.0
+CPFAC := 0.5 1.0 1.5 2.0
+UTILS := 0.5 1.0 2.0 4.0 8.0 16.0 24.0
 
 # Schedulability Analysis
-CORES := 2 4 8 12 16 20 24 32
-CORES := 2 4
+CORES := 2 4 8 12 16 20 24 28 32
