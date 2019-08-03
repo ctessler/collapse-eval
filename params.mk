c65527bc97242bcea39abd50469254c973b2c863
Corey Tessler <corey.tessler@wayne.edu>
2019-08-02 21:13:10 -0400

    Task creation parameters are useful

--- params.mk
+++ params.mk
@@ -3,16 +3,18 @@ JOBS := 4
 TIMEOUT := 10
 
 # Pipeline Stage 1, Graph Creation
-NODES := 4 8 12 16 20 24 28 32
-EDGEP := 0.02 0.06 0.10 0.14 0.18
+NODES := 16 32 64
+EDGEP := 0.02 0.06 0.12
 GRAFC := 20
+GRAFC := 5
 
 # Pipeline Stage 1, Demand Parameters
 WCET1 := 50
-OBJS  := 4 8 12 14 16
-GROWF := 0.2 0.4 0.6 0.8 1.0
-CPFAC := 0.5 1.0 1.5 2.0
-UTILS := 0.5 1.0 2.0 4.0 8.0 16.0 24.0
+OBJS  := 4 8 16
+GROWF := 0.2 0.6 1.0
+CPFAC := 0.5 1.5 2.5
+CPFAC := 0.5 2.5
+UTILS := 0.5 1.0 4.0 16.0
 
 # Schedulability Analysis
 CORES := 2 4 8 12 16 20 24 28 32
